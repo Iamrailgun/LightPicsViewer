17c8ce271118ba68db9f1bc92a7141c223cfe635
Iamrailgun <pombi4512@yandex.ru>
2019-10-07 17:56:46 +0300

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media.Imaging;
+
+namespace lpv
+{
+
+  public partial class MainWindow : Window
+  {
+
+    private string[] filesInDir;
+    private string[] images;
+    private int imgIdx = 0;
+    private Vector2D imgSize;
+    private Vector2D imgSizeTo;
+    private Vector2D imgMargin;
+    private Vector2D imgMarginTo;
+    private Vector2D mouseImgPos;
+    private Vector2D mousePos;
+    private double maxImgHeight;
+    private double scaleRate;
+    private double resizeSpeed;
+    private double moveSpeed;
+    private double minImgHeight;
+    private int refreshRate;
+    private double maxImgHeightValue;
+    private bool isMove = false;
+    private Thread thread;
+    private double deltaTime;
+    private readonly string[] exts = { "bmp", "dib", "rle", "jpg", "jpeg", "jpe", "jfif", "gif", "emf", "wmf", "tif", "tiff", "png", "ico" };
+
+    public MainWindow()
+    {
+      InitializeComponent();
+    }
+
+    private void Window_Loaded(object sender, RoutedEventArgs e)
+    {
+      scaleRate = Properties.Settings.Default.scaleRate;
+      resizeSpeed = Properties.Settings.Default.resizeSpeed;
+      moveSpeed = Properties.Settings.Default.moveSpeed;
+      refreshRate = Properties.Settings.Default.refreshRate;
+      maxImgHeightValue = Properties.Settings.Default.maxImgHeightValue;
+      minImgHeight = Properties.Settings.Default.minImgHeight;
+      Background.Opacity = Properties.Settings.Default.backgroundDim;
+      var args = Application.Current.FindResource("args") as string[];
+      try
+      {
+        filesInDir = Directory.GetFiles(Path.GetDirectoryName(args[0]));
+      }
+      catch (Exception)
+      {
+        Application.Current.Shutdown();
+      }
+      images = filesInDir.Where(f => exts.Contains(f.Split('.').Last())).ToArray();
+      imgIdx = Array.IndexOf(images, args[0]);
+      maxImgHeight = Height * maxImgHeightValue;
+      deltaTime = 1.0 / refreshRate;
+      ShowImage(images[imgIdx]);
+      thread = new Thread(new ThreadStart(Updata));
+      thread.Start();
+    }
+
+    private void Updata()
+    {
+      while (true)
+      {
+        Thread.Sleep(1000 / refreshRate);
+        image.Dispatcher.Invoke(new Action(UpdateImage));
+      }
+    }
+    private void UpdateImage()
+    {
+      if (isMove)
+      {
+        imgMarginTo = mousePos - mouseImgPos;
+        imgMargin = imgMargin.Lerp(imgMarginTo, moveSpeed * deltaTime);
+        image.Margin = new Thickness(imgMargin.X, imgMargin.Y, 0, 0);
+        imgSizeTo = imgSize;
+      }
+      else
+      {
+        imgSize = imgSize.Lerp(imgSizeTo, resizeSpeed * deltaTime);
+        image.Width = imgSize.X;
+        image.Height = imgSize.Y;
+        imgMargin = imgMargin.Lerp(imgMarginTo, resizeSpeed * deltaTime);
+        image.Margin = new Thickness(imgMargin.X, imgMargin.Y, 0, 0);
+      }
+    }
+
+    private void ShowImage(string imgPath)
+    {
+      try
+      {
+        var img = new BitmapImage(new Uri(imgPath));
+        var aspectRatio = img.Height / img.Width;
+        if (img.Height > maxImgHeight)
+        {
+          imgSize = new Vector2D(maxImgHeight / aspectRatio, maxImgHeight);
+        }
+        else
+        {
+          imgSize = new Vector2D(img.Width, img.Height);
+        }
+        if (img.Height < minImgHeight)
+        {
+          imgSize = new Vector2D(minImgHeight / aspectRatio, minImgHeight);
+        }
+        imgSizeTo = imgSize;
+        image.Width = imgSize.X;
+        image.Height = imgSize.Y;
+        var left = Width / 2 - imgSize.X / 2;
+        var top = Height / 2 - imgSize.Y / 2;
+        imgMargin = new Vector2D(left, top);
+        imgMarginTo = imgMargin;
+        image.Margin = new Thickness(left, top, 0, 0);
+        image.Source = img;
+        image.Visibility = Visibility.Visible;
+      }
+      catch (Exception)
+      {
+        Application.Current.Shutdown();
+      }
+    }
+
+    private void NextImage()
+    {
+      image.Visibility = Visibility.Hidden;
+      imgIdx++;
+      if (imgIdx > images.Length - 1)
+      {
+        imgIdx = 0;
+      }
+      ShowImage(images[imgIdx]);
+    }
+
+    private void PrevImage()
+    {
+      image.Visibility = Visibility.Hidden;
+      imgIdx--;
+      if (imgIdx < 0)
+      {
+        imgIdx = images.Length - 1;
+      }
+      ShowImage(images[imgIdx]);
+    }
+
+    private void Window_MouseWheel(object sender, MouseWheelEventArgs e)
+    {
+      var miPos = new Vector2D(Mouse.GetPosition(image));
+      var prevSize = imgSize;
+      if (e.Delta > 0 && !isMove)
+      {
+        imgSizeTo = prevSize * (1 + scaleRate);
+        imgMarginTo = imgMargin - (miPos * (1 + scaleRate) - miPos);
+      }
+      else if (imgSize.Y > minImgHeight)
+      {
+        imgSizeTo = prevSize * (1 - scaleRate);
+        imgMarginTo = imgMargin - (miPos * (1 - scaleRate) - miPos);
+      }
+    }
+
+    private void Window_KeyDown(object sender, KeyEventArgs e)
+    {
+      if (e.Key == Key.Escape)
+      {
+        Application.Current.Shutdown();
+      }
+      if (e.Key == Key.Right)
+      {
+        NextImage();
+      }
+      if (e.Key == Key.Left)
+      {
+        PrevImage();
+      }
+      if (e.Key == Key.R)
+      {
+        ShowImage(images[imgIdx]);
+      }
+    }
+
+    private void Image_MouseDown(object sender, MouseButtonEventArgs e)
+    {
+      if (e.LeftButton == MouseButtonState.Pressed)
+      {
+        mouseImgPos = new Vector2D(e.GetPosition(image));
+        isMove = true;
+      }
+    }
+
+    private void Window_MouseUp(object sender, MouseButtonEventArgs e)
+    {
+      if (e.LeftButton == MouseButtonState.Released)
+      {
+        isMove = false;
+      }
+    }
+
+    private void Window_MouseMove(object sender, MouseEventArgs e)
+    {
+      mousePos = new Vector2D(e.GetPosition(this));
+    }
+
+    private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+    {
+      thread.Abort();
+    }
+
+    private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+    {
+      if (e.LeftButton == MouseButtonState.Pressed)
+      {
+        var p = new Vector2D(e.GetPosition(this));
+        if (!(imgMargin.X < p.X && 
+              imgMargin.Y < p.Y && 
+              imgMargin.X + imgSize.X > p.X && 
+              imgMargin.Y + imgSize.Y > p.Y))
+        {
+          Application.Current.Shutdown();
+        }
+      }
+      if (e.ChangedButton == MouseButton.XButton1)
+      {
+        PrevImage();
+      }
+      if (e.ChangedButton == MouseButton.XButton2)
+      {
+        NextImage();
+      }
+    }
+  }
+}
---
+++ Vector2D.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Windows;
+
+namespace lpv
+{
+  class Vector2D
+  {
+    public double X { get; set; }
+    public double Y { get; set; }
+
+    public Vector2D(double X, double Y)
+    {
+      this.X = X;
+      this.Y = Y;
+    }
+
+    public Vector2D(Point point)
+    {
+      this.X = point.X;
+      this.Y = point.Y;
+    }
+
+    public Vector2D Add(Vector2D v2d) => new Vector2D(this.X + v2d.X, this.Y + v2d.Y);
+
+    public Vector2D Sub(Vector2D v2d) => new Vector2D(this.X - v2d.X, this.Y - v2d.Y);
+
+    public Vector2D Neg() => new Vector2D(-1 * this.X, -1 * this.Y);
+
+    public static Vector2D operator +(Vector2D v2da, Vector2D v2db) => v2da.Add(v2db);
+
+    public static Vector2D operator -(Vector2D v2da, Vector2D v2db) => v2da.Sub(v2db);
+
+    public static Vector2D operator -(Vector2D v2d) => v2d.Neg();
+
+    public static Vector2D operator *(Vector2D v2d, double d) => new Vector2D(v2d.X * d, v2d.Y * d);
+
+    public static Vector2D operator *(double d, Vector2D v2d) => v2d * d;
+
+    public Vector2D Lerp(Vector2D to, double amount) => this + (to - this) * amount;
+
+    public static Vector2D Lerp(Vector2D from, Vector2D to, double amount) => from.Lerp(to, amount);
+
+    public static bool operator !=(Vector2D v2da, Vector2D v2db)
+    {
+      return !(v2da.X == v2db.X && v2db.Y == v2db.Y);
+    }
+
+    public static bool operator ==(Vector2D v2da, Vector2D v2db)
+    {
+      return v2da.X == v2db.X && v2db.Y == v2db.Y;
+    }
+
+    public override string ToString()
+    {
+      return $"X:{X} Y:{Y}";
+    }
+
+    public override bool Equals(object obj) => obj is Vector2D v2d &&
+             X == v2d.X &&
+             Y == v2d.Y;
+
+    public override int GetHashCode()
+    {
+      var hashCode = 1861411795;
+      hashCode = hashCode * -1521134295 + X.GetHashCode();
+      hashCode = hashCode * -1521134295 + Y.GetHashCode();
+      return hashCode;
+    }
+  }
+}
